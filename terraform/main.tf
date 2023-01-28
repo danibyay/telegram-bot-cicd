6feeb21c1893f94343327096ec0b8dbb13da5fe6
Daniela Becerra <danybecerr@gmail.com>
2023-01-27 22:16:48 -0600

    improve syntax 3

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,7 @@ resource "azurerm_container_group" "aci" {
   name                = "danibcontainergroup"
   resource_group_name = data.azurerm_resource_group.rg.name
   location            = "canadacentral"
-  ip_address_type     = "public"
+  ip_address_type     = "Public"
 
   os_type             = "Linux"
 
